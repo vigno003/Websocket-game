1bbd35b42e6bd9f3afcca957fd8d6d723e76cc00
Zaidaman <73349474+Zaidaman@users.noreply.github.com>
2022-04-29 15:29:20 +0200

    Update wsClient.cs

--- client/wsClient.cs
+++ client/wsClient.cs
@@ -4,24 +4,13 @@ let target = {x:0,y:0};
 
 serverConnection.onopen = function() {
   console.log("connected "+serverAddress);
+  serverConnection.send("#1");
 }
 
 function sendCoordinates() {
-  let norm ={
-    x:target.x/width,
-    y:target.y/height
-  }
-  let str=JSON.stringify(norm);
-  //serverConnection.onopen = () => serverConnection.send(str);
-  serverConnection.send(str);
+  
 }
 
 serverConnection.onmessage = function(event) {
-  console.log("move: "+event.data)
-  let obj=JSON.parse(event.data);
-  client=
-  obj.x*=width;
-  obj.y*=height;
-  target.x=obj.x;
-  target.y=obj.y;
+  
 }
