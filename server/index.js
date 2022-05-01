e54de700983dfc677a115bd898a40bc7271c8738
Zaidaman <73349474+Zaidaman@users.noreply.github.com>
2022-05-01 12:01:37 +0200

    changed the code

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const Websocket = require('ws')
 const port=8080;
 const wsServer = new WebSocket.Server({port:PORT})
 var palyersData = {"type": "playerData"}
+Random rnd = new Random();
 
 wsServer.on('open'){
     console.log("The server is listening on port " + wsServer.options.port);
@@ -9,11 +10,21 @@ wsServer.on('open'){
 
 
 wsServer.on('connection', function(client,req){
-    client.id = 
+    
+    client.id = rnd.Next(1,100)*rnd.Next(1,100);
+    playersData["" + client.id] = {id : client.id}
+    client.send(`{"id": "${client.id}"`)
   
-    client.on('message', function(msg){
-          wsServer.clients.forEach(function (clientss){
-          clientss.send(""+msg);
+    client.on('message', (msg) => {
+            var dataJSON = JSON.parse(data)
+    
+            console.log("Player Message")
+            console.log(dataJSON)
+        })
+        
+        client.on('close', () => {
+            console.log("Connection Closed")
+            console.log("Log out client: "+ client.id)
         })
     })
 })
