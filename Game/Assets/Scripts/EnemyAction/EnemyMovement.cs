4199601bfd4fe445f991d479e23c23d0c68d8e13
Luca Vignali <lucavigno2003@gmail.com>
2022-05-01 16:00:57 +0200

    Aggiornato il server e il gioco

---
+++ Game/Assets/Scripts/EnemyAction/EnemyMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public int EnemySpeed;
+    GameObject m_player;
+
+    
+    private void Awake()
+    {
+        m_player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 localPosition = m_player.transform.position - transform.position;
+        localPosition = localPosition.normalized;
+        transform.Translate(localPosition.x * Time.deltaTime * EnemySpeed, 
+                            0f, 
+                            localPosition.z * Time.deltaTime * EnemySpeed);
+    }
+}
---
+++ Game/Assets/Scripts/GameAction/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ConnectButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
